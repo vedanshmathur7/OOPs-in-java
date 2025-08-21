cf6622eeea2ed4bec3b4ac29895f823df7d24d69
vedanshmathur7 <vedanshmathur007@gmail.com>
2025-08-21 15:25:09 +0530

    Create _2_sum_of_natural_no.java

---
+++ Lecture 4 (loops)/_2_sum_of_natural_no.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class _2_sum_of_natural_no {
+    public static void main(String[] args) {
+        
+        int sum=0;
+
+        System.out.println("Enter the range: ");
+        Scanner sc = new Scanner (System.in);
+        int range = sc.nextInt();
+        // System.out.println("lk");
+
+        for (int i =1; i<=range; i++){
+            System.out.println(i);
+            // System.out.println);
+        }
+
+        System.out.println("The sum is: "+sum);
+        sc.close();
+    }
+}
