267013561c0cde9c19645e02832a85ae377bdad1
vedanshmathur7 <vedanshmathur007@gmail.com>
2025-08-28 16:04:06 +0530

    Create _7_f1_map.java

---
+++ Lecture 6 (patterns)/_7_f1_map.java
@@ -0,0 +1,33 @@
+public class _7_f1_map {
+
+    public static void main(String[] args) {
+        int n = 5;
+        int nsp = 0;
+        int nst = n;
+        int row = 1;
+
+        while (row <= n) {
+
+            int csp = 1;
+
+            while (csp <= nsp) {
+                System.out.print("   ");
+                csp++;
+            }
+
+            int cst = 1;
+
+            while (cst <= nst) {
+                System.out.print("*  ");
+                cst++;
+            }
+
+            nsp += 2;
+            nst -= 1;
+            row += 1;
+
+            System.out.println();
+        }
+    }
+}
+
